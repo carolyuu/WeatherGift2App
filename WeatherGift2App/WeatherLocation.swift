bfc8b53258531c81575ffa2fef5c17dc3c78399e
Xiaotong <yuafq@bc.edu>
2022-03-26 19:00:11 -0400

    Completed 6.2

---
+++ WeatherGift2App/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift2App
+//
+//  Created by Carol Yu on 3/26/22.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
