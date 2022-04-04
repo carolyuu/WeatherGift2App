e7bd10e81af17ca19b5e36eb9c7d5cda748076da
Xiaotong <yuafq@bc.edu>
2022-04-03 23:13:21 -0400

    completed weather gift app

---
+++ WeatherGift2App/AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  WeatherGift2App
+//
+//  Created by Carol Yu on 4/3/22.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
